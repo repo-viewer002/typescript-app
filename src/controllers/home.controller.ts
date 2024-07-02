1ba8dbe47305c0ed2a4413defafaedefda8b6552
samm002 <samuelsdjodis@gmail.com>
2024-07-02 18:42:43 +0700

    refactor(route): refactoring default api route

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export default {
   home(req: Request, res: Response) {
     try {
-      res.status(200).send("<h1>Welcome to my API!</h1>");
+      res.status(200).send("<h1>Welcome to my API!</h1><h3>Repository link : <a href=\"https://github.com/repo-viewer002/typescript-app.git\">typescript-app</a></h3>");
 
     } catch (error) {
       const _err = error as Error;
